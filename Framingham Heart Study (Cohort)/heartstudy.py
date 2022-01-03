af67c7aabd4b5f3584fbb94b85431bf9e6230c09
meRveet <31720342+meRveet@users.noreply.github.com>
2022-01-03 22:24:36 +0800

    included scripts for profiler

---
+++ Framingham Heart Study (Cohort)/heartstudy.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+from pandas_profiling import ProfileReport as pr
+
+df = pd.read_csv('D:\\Dropbox\\PythonProjects\\pythonprojects\\Framingham Heart Study (Cohort)\\framingham.txt')
+df
+
+#Using profiler to quickly explore the dataset
+profile = pr(df, title= 'Heart Dataset', html={'style':{'full_width':True}})
+profile
+
+
