6bb4b772588170a99b0f12fa4747218272504944
meRveet <31720342+meRveet@users.noreply.github.com>
2022-01-06 23:54:45 +0800

    udpated dataset. cleaning up in progress

--- Heart Study (Cohort)/heartstudy.py
+++ Heart Study (Cohort)/heartstudy.py
@@ -5,8 +5,6 @@ from pandas_profiling import ProfileReport as pr
 df = pd.read_csv('D:\\Dropbox\\PythonProjects\\pythonprojects\\Heart Study (Cohort)\\fulldata.csv')
 df
 
-#Using profiler to quickly explore the dataset
-profile = pr(df, title= 'Heart Dataset', html={'style':{'full_width':True}})
+# Using profiler to quickly explore the dataset
+profile = pr(df, title='Heart Dataset', html={'style': {'full_width': True}})
 profile
-
-
